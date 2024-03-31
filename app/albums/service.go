0907b349a7f995459ab41ebb42e0988753be8cdb
julian-saleix <julian.saleix@gmail.com>
2024-03-31 23:33:48 +0200

    chore: adding docker compose file + moving app files in app/ folder

--- app/albums/service.go
+++ app/albums/service.go
@@ -2,35 +2,35 @@ package albums
 
 import "github.com/google/uuid"
 
-var albums = []album{
+var albums = []Album{
 	{ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
 	{ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
 	{ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
 }
 
-func getAlbums() []album {
+func getAlbums() []Album {
 	return albums
 }
 
-func postAlbums(data CreateAlbumDto) (response album, ok bool) {
-	var newAlbum album
+func postAlbums(data CreateAlbumDto) (response Album, ok bool) {
+	var newAlbum Album
 	if data.Title == "" || data.Artist == "" {
 		return newAlbum, false
 	} else {
 		newId := uuid.New().String()
-		newAlbum = album{Title: data.Title, Artist: data.Artist, Price: data.Price, ID: newId}
+		newAlbum = Album{Title: data.Title, Artist: data.Artist, Price: data.Price, ID: newId}
 		albums = append(albums, newAlbum)
 		return newAlbum, true
 	}
 
 }
 
-func getAlbumById(id string) (res album, ok bool) {
+func getAlbumById(id string) (res Album, ok bool) {
 	for _, a := range albums {
 		if a.ID == id {
 			return a, true
 		}
 	}
 
-	return album{}, false
+	return Album{}, false
 }
--- readme.md
+++ readme.md
@@ -1,16 +1,19 @@
 # go gin
+
 Project to practice and familiarise with Golang and the Gin Web Framework.
 Also implementing Server-sent-events.
 
 ## Requirements
 
-- ```Go```
+-   `Go`
 
 ## Starting the project
 
 Run the following command:
+
 ```
-cp .env.example .env 
-go get . 
-go run . 
+cd app/
+cp .env.example .env
+go get .
+go run .
 ```
